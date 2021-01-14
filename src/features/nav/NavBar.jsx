eb9b66844203228768fb4b07882fd27744ccc837
pedroobando <pedroobando@hotmail.com>
2021-01-14 07:34:25 -0400

    NavLink and Link

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
 
 const NavBar = ({ setFormOpen }) => {
   return (
     <Menu fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} exact to="/" header>
           <img src="/assets/logo.png" alt="logo" style={{ marginRight: '15px' }} />
           Re-Vents
         </Menu.Item>
-        <Menu.Item name="Event" />
-        <Menu.Item>
+        <Menu.Item as={NavLink} to="/events" name="Event" />
+        <Menu.Item as={NavLink} to="/createEvent">
           <Button onClick={setFormOpen} positive inverted content="Create Event" />
         </Menu.Item>
         <Menu.Item position="right">
