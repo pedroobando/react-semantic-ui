3fbf4bc93d950496605ed856a7f9dabdb75d8741
pedroobando <pedroobando@hotmail.com>
2021-01-14 06:37:34 -0400

    Routing configuration

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import EventDatailedPage from '../../features/events/eventDetailed/EventDatailed';
+import EventForm from '../../features/events/eventForm/EventForm';
+import HomePage from '../../features/home/HomePage';
 import NavBar from '../../features/nav/NavBar';
 
 import './styles.css';
@@ -25,12 +29,17 @@ const App = () => {
     <>
       <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/events" component={EventDashboard} />
+        <Route path="/events/:id" component={EventDatailedPage} />
+        <Route path="/createEvent" component={EventForm} />
+
+        {/* <EventDashboard
           formOpen={formOpen}
           setFormOpen={setFormOpen}
           selectEvent={handleSelectEvent}
           selectedEvent={selectedEvent}
-        />
+        /> */}
       </Container>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import './index.css';
-// import './app/layout/styles.css';
 import App from './app/layout/App';
-// import reportWebVitals from "./reportWebVitals";
 import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
-render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootEl
+);
