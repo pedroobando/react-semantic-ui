b34f0463ecc5c8716f3b285629099c233565a87f
pedroobando <pedroobando@hotmail.com>
2021-02-11 17:46:45 -0400

    Form validation

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Button, Header, Segment, FormField } from 'semantic-ui-react';
+import { Button, Header, Segment, FormField, Label } from 'semantic-ui-react';
 import cuid from 'cuid';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { createEvent, updateEvent } from '../eventActions';
+import * as Yup from 'yup';
 
 const EventForm = ({ match, history }) => {
   const dispatch = useDispatch();
@@ -20,42 +21,57 @@ const EventForm = ({ match, history }) => {
     venue: '',
     date: '',
   };
-  const [values, setValues] = useState(initialValues);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
+  const validationSchema = Yup.object({
+    title: Yup.string().required('You must provide a title'),
+  });
+  // const [values, setValues] = useState(initialValues);
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    selectedEvent
-      ? dispatch(updateEvent({ ...selectedEvent, ...values }))
-      : dispatch(
-          createEvent({
-            ...values,
-            id: cuid(),
-            hostedBy: 'Bob',
-            attendees: [
-              {
-                id: cuid(),
-                photoURL: '/assets/user.png',
-                name: 'moli',
-              },
-            ],
-            hostPhotoURL: '/assets/user.png',
-          })
-        );
-    history.push('/events');
-  };
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setValues({ ...values, [name]: value });
+  // };
+
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
+  //   selectedEvent
+  //     ? dispatch(updateEvent({ ...selectedEvent, ...values }))
+  //     : dispatch(
+  //         createEvent({
+  //           ...values,
+  //           id: cuid(),
+  //           hostedBy: 'Bob',
+  //           attendees: [
+  //             {
+  //               id: cuid(),
+  //               photoURL: '/assets/user.png',
+  //               name: 'moli',
+  //             },
+  //           ],
+  //           hostPhotoURL: '/assets/user.png',
+  //         })
+  //       );
+  //   history.push('/events');
+  // };
 
   return (
     <Segment clearing>
       <Header content={!selectedEvent ? 'Create new event' : 'Edit the event'} />
-      <Formik initialValues={initialValues} onSubmit={(values) => console.log(values)}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values) => console.log(values)}>
         <Form className="ui form">
           <FormField>
             <Field name="title" placeholder="Event title" />
+            <ErrorMessage
+              name="title"
+              render={(error) => (
+                <Label basic color="red">
+                  {error}
+                </Label>
+              )}
+            />
           </FormField>
           <FormField>
             <Field name="category" placeholder="Category" />
