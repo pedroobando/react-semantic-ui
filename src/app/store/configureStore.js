7120b6de28d9bd33c940292179f43f914e1ea770
pedroobando <pedroobando@hotmail.com>
2021-03-24 09:56:21 -0400

    AsyncReducer - Implements

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,10 +1,15 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+// solo implementa sin los middlewate
+// import { devToolsEnhancer } from 'redux-devtools-extension';
+
+// solo se usa cuando se implementa un middleware en este caso es thunk
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
+import thunk from 'redux-thunk';
 
 export const configureStore = () => {
   // return createStore(rootReducer);
-  return createStore(rootReducer, devToolsEnhancer());
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 };
 
 export default configureStore;
